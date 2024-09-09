fc1847e7a52c76e56e7e643fc04fa160999eab63
Michael Winsor <mgwinsor@gmail.com>
2024-09-09 21:58:30 +0800

    update: python lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -167,7 +167,21 @@ return {
       local servers = {
         -- clangd = {},
         gopls = {},
-        pyright = {},
+        pyright = {
+          settings = {
+            pyright = {
+              disableOrganizeImports = true,
+            },
+            python = {
+              analysis = {
+                ignore = { '*' },
+                typeCheckingMode = 'off',
+              },
+              venvPath = '.',
+              venv = '.venv',
+            },
+          },
+        },
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
