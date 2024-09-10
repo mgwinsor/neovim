fae6cfd9ba1daf2358de5bdd36b4565f7fc7a9f7
Michael <mgwinsor@gmail.com>
2024-09-10 11:54:07 +0800

    fix: harpoon keybindings

--- lua/plugins/keys.lua
+++ lua/plugins/keys.lua
@@ -1,16 +1,16 @@
 return {
-  "folke/which-key.nvim",
-  event = "VimEnter", -- Sets the loading event to 'VimEnter'
+  'folke/which-key.nvim',
+  event = 'VimEnter', -- Sets the loading event to 'VimEnter'
   opts = {
-    preset = "modern",
+    preset = 'modern',
   },
   spec = {
-    { "<leader>c", group = "[C]ode" },
-    { "<leader>d", group = "[D]ocument" },
-    { "<leader>r", group = "[R]ename" },
-    { "<leader>s", group = "[S]earch" },
-    { "<leader>t", group = "[T]oggle" },
-    { "<leader>w", group = "[W]orkspace" },
-    { "<leader>h", group = "Git [H]unk", mode = { "n", "v" } },
+    { '<leader>c', group = '[C]ode' },
+    { '<leader>d', group = '[D]ocument' },
+    { '<leader>r', group = '[R]ename' },
+    { '<leader>s', group = '[S]earch' },
+    { '<leader>t', group = '[T]oggle' },
+    { '<leader>w', group = '[W]orkspace' },
+    { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
   },
 }
