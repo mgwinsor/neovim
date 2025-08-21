45ee831126c9e617474963d01724d177e99a3c0a
Michael Winsor <mgwinsor@gmail.com>
2025-08-21 23:54:48 +0800

    fix: tmux nav config

--- lua/plugins/tmux.lua
+++ lua/plugins/tmux.lua
@@ -1,15 +1,15 @@
 return {
-  "christoomey/vim-tmux-navigator",
+  'christoomey/vim-tmux-navigator',
   cmd = {
-    "TmuxNavigateLeft",
-    "TmuxNavigateDown",
-    "TmuxNavigateUp",
-    "TmuxNavigateRight",
+    'TmuxNavigateLeft',
+    'TmuxNavigateDown',
+    'TmuxNavigateUp',
+    'TmuxNavigateRight',
   },
   keys = {
-    { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
-    { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
-    { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
-    { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+    { '<c-h>', '<cmd>TmuxNavigateLeft<cr>' },
+    { '<c-j>', '<cmd>TmuxNavigateDown<cr>' },
+    { '<c-k>', '<cmd>TmuxNavigateUp<cr>' },
+    { '<c-l>', '<cmd>TmuxNavigateRight<cr>' },
   },
 }
