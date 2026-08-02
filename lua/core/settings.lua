7902222da02169dd8ccfe6830daf215892550d15
Michael Winsor <mgwinsor@gmail.com>
2026-08-03 00:11:37 +0800

    fix: auto indent

--- lua/core/settings.lua
+++ lua/core/settings.lua
@@ -29,6 +29,11 @@ vim.o.spell = true
 vim.o.spelllang = 'en_us'
 vim.o.winblend = 10
 
+vim.o.expandtab = true
+vim.o.shiftwidth = 2
+vim.o.tabstop = 2
+vim.o.softtabstop = 2
+
 -- Sync clipboard between OS and Neovim.
 --  Schedule the setting after `UiEnter` because it can increase startup-time.
 vim.schedule(function()
